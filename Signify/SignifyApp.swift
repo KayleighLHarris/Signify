6bb0f69889957e2cdc8941374a3b1429692d7c7a
KayleighLHarris <x19471636@student.ncirl.ie>
2023-05-10 13:05:48 +0100

    Initial Commit

---
+++ Signify/SignifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignifyApp.swift
+//  Signify
+//
+//  Created by Kayleigh Harris on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SignifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
